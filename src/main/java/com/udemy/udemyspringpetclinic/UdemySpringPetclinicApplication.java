8e02c25783e7264c3e2d7122d5dafc9d76272bb7
liliyafedyushko <liilya.fedyushko@vitech.com.ua>
2020-03-25 22:02:21 +0200

    initial commit

---
+++ src/main/java/com/udemy/udemyspringpetclinic/UdemySpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.udemyspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringPetclinicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UdemySpringPetclinicApplication.class, args);
+  }
+
+}
